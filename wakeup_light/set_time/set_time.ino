9a9dbdb27bbb4b83a1aa7a3da938196297b519a5
Sander Beekhuis <sander.beekhuis@live.nl>
2019-12-04 21:59:22 +0100

    Make set time work with shield-like mounted RTc

--- wakeup_light/set_time/set_time.ino
+++ wakeup_light/set_time/set_time.ino
@@ -4,9 +4,16 @@
 namespace {
   
 
-const int kCePin = 7;   // Chip Enable
-const int kIoPin = 6;   // Input/Output
-const int kSclkPin = 5; // Serial Clock
+// RTC Pins configured for position of RTC Over arduino board (shield-like)
+#define RTC_VCC 7
+#define RTC_GND 6
+#define RTC_CLK 5
+#define RTC_DAT 4
+#define RTC_RST 3
+
+const int kCePin = RTC_RST;   // Chip Enable
+const int kIoPin = RTC_DAT;   // Input/Output
+const int kSclkPin = RTC_CLK; // Serial Clock
 
 // Create a DS1302 object.
 DS1302 rtc(kCePin, kIoPin, kSclkPin);
@@ -41,14 +48,14 @@ void printTime()
 void setup()
 {
     Serial.begin(9600);
-    pinMode(3, OUTPUT);
-    digitalWrite(3, HIGH); // VCC
-    pinMode(4, OUTPUT);
-    digitalWrite(4, LOW); // GND
+    pinMode(RTC_VCC, OUTPUT);
+    digitalWrite(RTC_VCC, HIGH); // VCC
+    pinMode(RTC_GND, OUTPUT);
+    digitalWrite(RTC_GND, LOW); // GND
     rtc.writeProtect(false);
     rtc.halt(false);
     // Donnerstag, 1. September, 10:00:00
-    Time t(2016, 9, 1, 10, 0, 0, Time::kTuesday);
+    Time t(2021, 12, 5, 21, 55, 0, Time::kWednesday);
     rtc.time(t);
 }
 
